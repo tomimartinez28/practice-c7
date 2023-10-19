2be1cac53cff8d713c8b6682c7d41dd56d554a89
Tomas Martinez <tomasagustinmartinez28@gmail.com>
2023-10-19 19:34:35 -0300

    moving files

--- ejercicios/05-repaso/banco/test.py
+++
@@ -1,4 +0,0 @@
-USUARIOS = [ {'username' : 'tomimartinez28','password' : 'heladito123', 'saldo' : 0 },
-            {'username' : 'zoe28','password' : 'heladito456', 'saldo' : 0 },
-            {'username' : 'lea28','password' : 'heladito789', 'saldo' : 10 },
-]
